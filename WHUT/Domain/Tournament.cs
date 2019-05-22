3baa9f78eeec3b72e9b2a96e0dad083b9f868f59
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2019-05-22 15:01:54 +0200

    Fixet

--- WHUT/Domain/Tournament.cs
+++ WHUT/Domain/Tournament.cs
@@ -33,18 +33,6 @@ namespace WHUT.Domain
         public string Location { get; private set; }
         public DateTime Date { get; private set; }
 
-        private List<Player> participants = new List<Player> {
-                new Player("Kenneth Mortensen", "Odense", "Zarbags's Gitz", 6, 0, 73),
-                new Player("Mike Larsen", "Danmark", "Godsworn Hunt", 0, 4, -32),
-                new Player("Lau Steffensen", "Danmark", "Chosen Axes", 3, 2, -9),
-                new Player("Kim", "Danmark", "Stormsire's Cursebreakers", 6, 1, 7),
-                new Player("Libak", "Danmark", "Stormsire's Cursebreakers", 6, 0, 30)
-                };
-        public string SetRulesetVariant
-        {
-            get;
-            set;
-        }
         public Tournament(string name, string location, DateTime date)
         {
             Name = name;
