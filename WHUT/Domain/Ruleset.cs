d7690204099220b94cc5e7c95911b657e3ac5b70
LAPTOP-Kenneth <kennethdammortensen@gmail.com>
2019-05-22 10:00:14 +0200

    RoundRobin Done. Shuffle is acting up

--- WHUT/Domain/Ruleset.cs
+++ WHUT/Domain/Ruleset.cs
@@ -11,6 +11,12 @@ namespace WHUT.Domain
 
         public List<Player> SingleElimination(List<Player> players)
         {
+            //Check to see if it's the first round of play
+            if (CheckIfFirstRound(players))
+            {
+                return Shuffle(players);
+            }
+
             //Remove Players who lost a match
             foreach(Player player in players)
             {
@@ -24,10 +30,17 @@ namespace WHUT.Domain
             return Shuffle(players);
         }
 
+
         public (List<Player>, List<Player>) DoubleElimination(List<Player> upperPlayers, List<Player> lowerPlayers)
-        {           
+        {
+            //Check to see if it's the first round of play
+            if (lowerPlayers == null)
+            {
+                return Shuffle(upperPlayers);
+            }
+
             //Move all players with 1 loss to lower bracket
-            foreach(Player player in upperPlayers)
+            foreach (Player player in upperPlayers)
             {
                 if (player.Losses > 0)
                 {
@@ -44,7 +57,7 @@ namespace WHUT.Domain
                 }
             }
 
-            return (upperPlayers, lowerPlayers);
+            return (Shuffle(upperPlayers), Shuffle(lowerPlayers));
         }
 
         public List<Player> Swiss(List<Player> players, int parringtype)
@@ -53,6 +66,26 @@ namespace WHUT.Domain
         }
         public List<Player> RoundRobin(List<Player> players)
         {
+            //Check to see if it's the first round of play
+            if (CheckIfFirstRound(players))
+            {
+                //Mangler at added en dumme spiller hvis der er ulige antal
+                Shuffle(players);
+            }
+            else
+            {
+                //Push all players one space in the list while keeping player one in place. Ex. 1-2-3-4 to 1-4-2-3
+
+            }
+            //Pair players so player one meets the last player in the list and player two the second last. Ex 1-2-3-4-5-6. 1 vs 6, 2 vs 4.... 
+            List<Player> pairedPlayers = new List<Player>();
+            for (int i = 0; i < players.Count / 2; i++)
+            {
+                pairedPlayers.Add(players[i]);
+                pairedPlayers.Add(players[players.Count - i]);
+            }
+            
+
             return null;
         }
 
@@ -62,13 +95,27 @@ namespace WHUT.Domain
             List<Player> playersShuffled = new List<Player>();
             Random rand = new Random();
 
-            while(players.Count < 0)
+            for (int i = 0; i < players.Count; i++)
             {
                 int nextIndex = rand.Next(players.Count + 1);
                 playersShuffled.Add(players[nextIndex]);
             }
 
             return playersShuffled;
+        }
+        private bool CheckIfFirstRound(List<Player> players)
+        {
+            int result = 0;
+            players.ForEach(x => result += x.Score);
+
+            if(result == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         #endregion
     }
