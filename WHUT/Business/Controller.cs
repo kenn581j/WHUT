ad646c7a4ae2637fa3acc9100b44c4a74c153b10
Jimmy Martin Henriksen (Stationær PC) <jimm1034@edu.eal.dk>
2019-05-28 18:36:40 +0200

    .

--- WHUT/Business/Controller.cs
+++ WHUT/Business/Controller.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using WHUT.Persistence;
 
 namespace WHUT.Business
 {
     class Controller
     {
-        public XDocument LoadTournament(string text)
+        private TournamentRegistry tournamentRegistry = new TournamentRegistry();
+        private PlayerRegistry playerRegistry = new PlayerRegistry();
+        private PlayerRepo playerRepo = new PlayerRepo();
+
+        public XDocument LoadTournament(string tournamentName)
         {
-            throw new NotImplementedException();
+            XDocument tournamentLoaded = tournamentRegistry.LoadTournament(tournamentName);
+
+            return tournamentLoaded;
         }
     }
 }
