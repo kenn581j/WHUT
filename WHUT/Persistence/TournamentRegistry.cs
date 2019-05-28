2c367c6ccaacf0e2ab9ead7f6cdbd805401afa9b
Jimmy Martin Henriksen (Stationær PC) <jimm1034@edu.eal.dk>
2019-05-28 18:41:40 +0200

    Kan indlæse en oprettet turnering

--- WHUT/Persistence/TournamentRegistry.cs
+++ WHUT/Persistence/TournamentRegistry.cs
@@ -1,28 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using WHUT.Domain;
 
 namespace WHUT.Business
 {
     public class TournamentRegistry
     {
+        private string path = @"C:\Tournaments\";
+
+        private void CheckForDirectory()
+        {
+            if (Directory.Exists(path) == false)
+            {
+                DirectoryInfo tournamentsFolder = Directory.CreateDirectory(path);
+            }
+        }
         public void SaveTournament(Tournament tournament)
         {
-            XmlWriter xmlWriter = XmlWriter.Create(tournament.Name + ".xml");
+            CheckForDirectory();
+            XDocument saveTournament = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("Tournament",
 
-            xmlWriter.WriteStartDocument();
-            xmlWriter.WriteStartElement("Tournament");
-            
-            xmlWriter.WriteElementString("Name", tournament.Name);
-            xmlWriter.WriteElementString("Location", tournament.Location);
-            xmlWriter.WriteElementString("Date", tournament.Date.ToString());
-            xmlWriter.WriteEndElement();
+                    new XElement("Name", tournament.Name),
+                    new XElement("Location", tournament.Location),
+                    new XElement("Date", tournament.Date)
+                    )
 
-            xmlWriter.Close();
+                );
+            saveTournament.Save(path + tournament.Name + ".xml");
+        }
+
+        public XDocument LoadTournament(string tournament)
+        {
+            XDocument doc;
+            try
+            {
+                doc = XDocument.Load(path + tournament + ".xml");
+            }
+            catch
+            {
+                doc = null;
+            }
+            return doc;
         }
     }
 }
