7ae2d3467c7bbb31816a5010786f3eb36c9ad8dd
Jimmy Martin Henriksen (Stationær PC) <jimm1034@edu.eal.dk>
2019-05-28 20:22:10 +0200

    Merger manuelt fra master..

--- WHUT/Business/TournamentRepo.cs
+++ WHUT/Business/TournamentRepo.cs
@@ -17,5 +17,53 @@ namespace WHUT.Business
             tournament = new Tournament(name, location, date);
             tournamentRegistry.SaveTournament(tournament);
         }
+        public void NewRound()
+        {
+            List<Player> participants = SortParticipants(tournament.TournamentType, tournament.Participants);
+
+            Round newRound = new Round();
+
+            int j = 0;
+            for (int i = 0; i < participants.Count / 2; i++)
+            {
+                Match match = new Match(participants[j], participants[++j]);
+                j++;
+                newRound.AddMatch(match);
+            }
+
+            AddRound(newRound);
+        }
+        public List<Player> SortParticipants(string tournamentType, List<Player> participants)
+        {
+            List<Player> sortParticipants = new List<Player>();
+            Ruleset ruleset = new Ruleset();
+            switch (tournamentType)
+            {
+                case "Monrad Swiss":
+                    sortParticipants = ruleset.Swiss(participants);
+                    break;
+                case "Games Workshop Swiss":
+                    sortParticipants = ruleset.GameWorkShopSwiss(participants);
+                    break;
+                case "Single Elimination":
+                    sortParticipants = ruleset.SingleElimination(participants);
+                    break;
+                case "Double Elimination":
+                    sortParticipants = ruleset.DoubleElimination(participants);
+                    break;
+                case "Round Robin":
+                    sortParticipants = ruleset.RoundRobin(participants);
+                    break;
+                default:
+                    tournamentType = null;
+                    break;
+            }
+
+            return sortParticipants;
+        }
+        public void AddRound(Round round)
+        {
+            tournament.rounds.Add(round);
+        }
     }
 }
