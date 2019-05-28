9671d4f9b8a532389c3f6af87f3ba0a075eb5978
Jimmy Martin Henriksen (Stationær PC) <jimm1034@edu.eal.dk>
2019-05-28 20:23:38 +0200

    player class merget

--- WHUT/Domain/Player.cs
+++ WHUT/Domain/Player.cs
@@ -8,28 +8,40 @@ namespace WHUT.Domain
 {
    public class Player
     {
-        public string Name {get; set;}
-        public string Club {get; set;}
-        public string Warband {get; set;}
-        public int Score {get; set;}
+        public string Name { get; set; }
+        public string Club { get; set; }
+        public string Warband { get; set; }
+        public int Score { get; set; }
         public int Wins { get; set; }
         public int Losses { get; set; }
-        public int TiebreakerLoss {get; set;}
-        public int TiebreakerGloryDiff {get; set;}
+        public int TiebreakerLoss { get; set; }
+        public int TiebreakerGloryDiff { get; set; }
 
         public Player(string name, string club, string warband)
         {
             Name = name;
             Club = club;
-            Warband = warband; 
-  
+            Warband = warband;
+
         }
+
+        //Pt indeholder konstruktør score og tiebreakers pga. stub listen
+        public Player(string name, string club, string warband, int score, int tieBreakerByLoss, int tieBreakerGloryDiff)
+        {
+            Name = name;
+            Club = club;
+            Warband = warband;
+            Score = score;
+            TiebreakerLoss = tieBreakerByLoss;
+            TiebreakerGloryDiff = tieBreakerGloryDiff;
+
+        }
+
 
         public override string ToString()
         {
             return $"{Name} {Club} {Warband}";
             //Så behøver du ikke at lave en lang streng :> 
         }
-
     }
 }
